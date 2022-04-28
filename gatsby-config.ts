1e3fc7b530434f536b000a87f6fbddac6c6fb8f5
martyna <martyna@imas.lt>
2022-04-28 22:54:19 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `React Ts Gatsby Boilerplate`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
