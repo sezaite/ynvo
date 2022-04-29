2acb2ecf8b5a46ccafe0dca2fbc484b5c6bcd1e3
martyna <martyna@imas.lt>
2022-04-29 17:12:30 +0300

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,41 @@
-import type { GatsbyConfig } from "gatsby"
+const path = require('path');
 
-const config: GatsbyConfig = {
+module.exports = {
   siteMetadata: {
-    title: `React Ts Gatsby Boilerplate`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `test`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [],
-}
-
-export default config
+  plugins: [`gatsby-plugin-styled-components`, 
+  `gatsby-plugin-sharp`, 
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Red Hat Display\:300,400,400i,600,700`],
+        display: 'swap'
+      }
+    },
+    {
+			resolve: 'gatsby-plugin-page-creator',
+			options: {
+				path: path.resolve(__dirname, 'src/pages'),
+				ignore: ['**/sections/*', '**/options/*', '**/elements/*'],
+			},
+		},
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.resolve(__dirname, 'src/assets/images'),
+      },
+    },
+    {
+			resolve: 'gatsby-plugin-resolve-src',
+			options: {
+				srcPath: path.resolve(__dirname, 'src'),
+			},
+		},
+  ]
+};
